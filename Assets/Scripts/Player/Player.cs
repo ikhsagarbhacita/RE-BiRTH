9631c0a85578665d1e97d42271936754de32278c
Achmad Aulia Irsyad <achmadauliairsyad@gmail.com>
2026-09-16 20:15:26 +0800

    Movement kiri kanan

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,8 +3,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [Header("Component References")]
+    public float speed;
     private Rigidbody2D rb;
+    private float InputX;
+    private Animator anim;
+    private SpriteRenderer spriteRenderer;
 
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,6 +24,30 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        InputX = Input.GetAxis("Horizontal");
+
+        if (InputX != 0)
+        {
+            anim.SetBool("isRunning", true);
+            anim.SetFloat("animSpeed", speed / 5f);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+
+        if (InputX > 0)
+        {
+            spriteRenderer.flipX = false; // Menghadap kanan
+        }
+        else if (InputX < 0)
+        {
+            spriteRenderer.flipX = true; // Menghadap kiri
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(InputX * speed, rb.linearVelocity.y); 
     }
 }
